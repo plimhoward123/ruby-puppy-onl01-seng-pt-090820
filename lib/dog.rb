ad6ae2eeb6b0bfe5c8b16b6216d79321104269b8
Plimon Howard V <plimon.howard@gmail.com>
2020-10-07 17:10:49 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,24 @@
-# Add your code here
+class Dog
+  @@all = []
+  attr_accessor :name
+
+  def initialize(name)
+    save(name)
+  end
+
+  def self.all
+    return @@all
+  end
+  def self.clear_all
+    @@all.clear
+    return @@all
+  end
+  def self.print_all
+    puts @@all
+  end
+
+  def save(name)
+    @name = name
+    @@all << self
+  end
+end
